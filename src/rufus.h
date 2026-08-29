fc779fba70668e08decbe83821c8ccb0e3fda437
Smu1zel <lyndenl25@yahoo.com>
2026-08-28 20:10:47 -0400

    Use ANSI stdio Fixes crash on USB insert. DD mode now works

--- src/rufus.h
+++ src/rufus.h
@@ -15,6 +15,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#ifndef __USE_MINGW_ANSI_STDIO
+#define __USE_MINGW_ANSI_STDIO 1
+#endif
+
 #include <assert.h>
 #include <windows.h>
 #include <malloc.h>
